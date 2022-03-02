eb9a7998f5cc4a0e1460b5c694787f36127fa20f
Ziv5ha <89574596+Ziv5ha@users.noreply.github.com>
2022-03-02 17:44:08 +0200

    added to ability to create a new quiz - WIP no fully functional

--- front/src/createQuiz/CreateQuiz.tsx
+++ front/src/createQuiz/CreateQuiz.tsx
@@ -1,10 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../global-components/Navbar';
+import Question from './components/Question';
 
 export default function CreateQuiz() {
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const [questionArr, setQuestionArr] = useState<Question[]>([
+    { question: '', rightAnswer: '', wrongAnswers: ['', '', ''] },
+  ]);
+  const [questionsElems, setQuestionsElems] = useState<JSX.Element[]>([
+    <Question
+      key={`question#0}`}
+      questionArr={questionArr}
+      setQuestionArr={setQuestionArr}
+      index={questionIndex}
+    />,
+  ]);
+
+  const addQuestion = () => {
+    setQuestionIndex((prev) => ++prev);
+    setQuestionArr((prev) => [
+      ...prev,
+      { question: '', rightAnswer: '', wrongAnswers: ['', '', ''] },
+    ]);
+    setQuestionsElems((prev) => [
+      ...prev,
+      <Question
+        key={`question#${questionArr.length}`}
+        questionArr={questionArr}
+        setQuestionArr={setQuestionArr}
+        index={questionIndex + 1}
+      />,
+    ]);
+  };
+  //   const createQuizFunc = (e) => {
+  //     e.preventDefault();
+  //     console.log(e);
+  //   };
   return (
     <div>
       <Navbar />
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log(e);
+          console.log(questionArr);
+        }}
+      >
+        {questionsElems}
+        <button type='submit'>Submit</button>
+      </form>
+      <button onClick={addQuestion}>Add question+</button>
     </div>
   );
 }
