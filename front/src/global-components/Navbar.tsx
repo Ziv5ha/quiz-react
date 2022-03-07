5f6e64f99f3873f99b9961f471e4ec96ae5850f8
Ziv5ha <89574596+Ziv5ha@users.noreply.github.com>
2022-03-07 16:39:18 +0200

    navbar styling

--- front/src/global-components/Navbar.tsx
+++ front/src/global-components/Navbar.tsx
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
+import './styles/navbar.css';
 
 export default function Navbar() {
+  const [open, setOpen] = useState('0px');
+
+  const openMenu = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    setOpen('300px');
+  };
+  const closeMenu = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    setOpen('0px');
+  };
   return (
     <nav>
-      <ul>
-        <li>
-          <Link to='/'>Home</Link>
-        </li>
-        <li>
-          <Link to='/quiz'>Take Quiz</Link>
-        </li>
-        <li>
-          <Link to='/createQuiz'>Create Quiz</Link>
-        </li>
+      <button onClick={openMenu} className='nav-btn hamburger'>
+        <i className='fa-solid fa-bars'></i>
+      </button>
+      <ul style={{ maxWidth: open, minWidth: open }} className='nav-menu'>
+        <button onClick={closeMenu} className='nav-btn x'>
+          <i className='fa-solid fa-xmark'></i>
+        </button>
+        {/* <li> */}
+        <Link to='/'>Home</Link>
+        {/* </li> */}
+        {/* <li> */}
+        <Link to='/quiz'>Take Quiz</Link>
+        {/* </li> */}
+        {/* <li> */}
+        <Link to='/createQuiz'>Create Quiz</Link>
+        {/* </li> */}
       </ul>
     </nav>
   );
